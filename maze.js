21f4bdb54d561416fe05398aeef3238fec26e9de
harsh99786 <harsh99786@gmail.com>
2023-04-23 14:37:31 +0530

    Added maze and some css

--- maze.js
+++ maze.js
@@ -1,14 +1,30 @@
-    // Get the grid and create the robot element
-    const grid = document.getElementById('grid');
     const robot = document.createElement('div');
     robot.classList.add('robot');
     robot.innerText = 'R';
-
-    // Set the robot's initial position to the top left corner of the grid
+    var numItems = 25;
+    var grid = document.getElementById("grid");
+    var maze = [
+      [1, 1, 0, 0, 1],
+      [0, 1, 1, 1, 0],
+      [0, 0, 1, 0, 1],
+      [0, 1, 1, 1, 1],
+      [1, 0, 0, 0, 1]
+    ];
+    
+    
+    for(var i = 0;i < 5;i++)
+    {
+      for(var j = 0; j < 5; j++){
+        var item = document.createElement("div");
+        if(maze[i][j] == 0)
+        item.className = "grid-item-valid";
+        else 
+        item.className = "grid-item-inValid";
+        grid.appendChild(item);
+      }
+    }
     let position = { x: 0, y: 0 };
     grid.children[position.y * 5 + position.x].appendChild(robot);
-
-    // Function to move the robot based on an instruction
     function moveRobot(instruction) {
   switch (instruction) {
     case 'L':
@@ -54,20 +70,12 @@
   }
 }
 
-// Function to run the sequence of instructions
 function runInstructions() {
-  // const instructions = document.getElementById('instructions').value.split(',');
-  // console.log(instructions.value);
-  // instructions.forEach((instruction) => {
-  //   moveRobot(instruction.trim());
-  // });
-  var res = "RDRDRDLLU";
+  var res = "RDRDDRRD";
   for(let i = 0 ; i<res.length;i++){
       console.log(res[i]);
       setTimeout(function() {
 moveRobot(res[i]);
 }, 1000*(i+1));
   }
-
-
 }
